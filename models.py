ac3b4c9eb68814a1001ab4fa80b3228b204860e5
Test Name <test@example.com>
2024-04-04 16:15:58 -0500

    does this work

---
+++ models.py
@@ -0,0 +1,89 @@
+from _incydr_sdk.agents.models import Agent
+from _incydr_sdk.agents.models import AgentsPage
+from _incydr_sdk.alert_rules.models.response import RuleDetails
+from _incydr_sdk.alert_rules.models.response import RuleUser
+from _incydr_sdk.alert_rules.models.response import RuleUsersList
+from _incydr_sdk.alerts.models.alert import AlertDetails
+from _incydr_sdk.alerts.models.alert import AlertSummary
+from _incydr_sdk.alerts.models.response import AlertQueryPage
+from _incydr_sdk.audit_log.models import AuditEventsPage
+from _incydr_sdk.cases.models import Case
+from _incydr_sdk.cases.models import CaseFileEvents
+from _incydr_sdk.cases.models import CasesPage
+from _incydr_sdk.customer.models import Customer
+from _incydr_sdk.departments.models import DepartmentsPage
+from _incydr_sdk.devices.models import Device
+from _incydr_sdk.devices.models import DevicesPage
+from _incydr_sdk.directory_groups.models import DirectoryGroup
+from _incydr_sdk.directory_groups.models import DirectoryGroupsPage
+from _incydr_sdk.file_events.models.event import FileEventV2
+from _incydr_sdk.file_events.models.event import User
+from _incydr_sdk.file_events.models.response import FileEventsPage
+from _incydr_sdk.file_events.models.response import SavedSearch
+from _incydr_sdk.trusted_activities.models import TrustedActivitiesPage
+from _incydr_sdk.trusted_activities.models import TrustedActivity
+from _incydr_sdk.user_risk_profiles.models import UserRiskProfile
+from _incydr_sdk.user_risk_profiles.models import UserRiskProfilesPage
+from _incydr_sdk.users.models import Role
+from _incydr_sdk.users.models import UpdateRolesResponse
+from _incydr_sdk.users.models import UserRole
+from _incydr_sdk.users.models import UsersPage
+from _incydr_sdk.watchlists.models.responses import ExcludedUsersList
+from _incydr_sdk.watchlists.models.responses import IncludedDepartment
+from _incydr_sdk.watchlists.models.responses import IncludedDepartmentsList
+from _incydr_sdk.watchlists.models.responses import IncludedDirectoryGroup
+from _incydr_sdk.watchlists.models.responses import IncludedDirectoryGroupsList
+from _incydr_sdk.watchlists.models.responses import IncludedUsersList
+from _incydr_sdk.watchlists.models.responses import Watchlist
+from _incydr_sdk.watchlists.models.responses import WatchlistMembersList
+from _incydr_sdk.watchlists.models.responses import WatchlistsPage
+from _incydr_sdk.watchlists.models.responses import WatchlistUser
+
+__all__ = [
+    "Agent",
+    "AgentsPage",
+    "AlertDetails",
+    "AlertSummary",
+    "AlertQueryPage",
+    "Case",
+    "CaseFileEvents",
+    "CasesPage",
+    "Customer",
+    "Device",
+    "DevicesPage",
+    "SavedSearch",
+    "FileEventsPage",
+    "FileEventV2",
+    "User",
+    "UsersPage",
+    "UserRole",
+    "Role",
+    "UpdateRolesResponse",
+    "RuleUsersList",
+    "RuleUser",
+    "RuleDetails",
+    "TrustedActivity",
+    "TrustedActivitiesPage",
+    "DepartmentsPage",
+    "DirectoryGroupsPage",
+    "DirectoryGroup",
+    "Watchlist",
+    "WatchlistsPage",
+    "WatchlistMembersList",
+    "ExcludedUsersList",
+    "IncludedUsersList",
+    "WatchlistUser",
+    "IncludedDepartmentsList",
+    "IncludedDepartment",
+    "IncludedDirectoryGroupsList",
+    "IncludedDirectoryGroup",
+    "AuditEventsPage",
+    "UserRiskProfilesPage",
+    "UserRiskProfile",
+]
+
+
+__locals = locals()
+for __name in __all__:
+    if not __name.startswith("__"):
+        setattr(__locals[__name], "__module__", "incydr.models")  # noqa
